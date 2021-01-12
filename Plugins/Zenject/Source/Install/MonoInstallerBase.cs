1ba869b4870654e8ac17a620d678edc79bf59354
Pavel Fadrhonc <pavel.fadrhonc@gmail.com>
2021-01-12 11:59:41 +0100

    [Extenject] adding support for Odin in Unity facing Installers

--- Plugins/Zenject/Source/Install/MonoInstallerBase.cs
+++ Plugins/Zenject/Source/Install/MonoInstallerBase.cs
@@ -9,7 +9,13 @@ namespace Zenject
     // We'd prefer to make this abstract but Unity 5.3.5 has a bug where references
     // can get lost during compile errors for classes that are abstract
     [DebuggerStepThrough]
-    public class MonoInstallerBase : MonoBehaviour, IInstaller
+    public class MonoInstallerBase :
+#if ODIN_INSPECTOR
+        SerializedMonoBehaviour
+#else
+        MonoBehaviour
+#endif
+        ,IInstaller
     {
         [Inject]
         protected DiContainer Container
--- Plugins/Zenject/Source/Install/ScriptableObjectInstallerBase.cs
+++ Plugins/Zenject/Source/Install/ScriptableObjectInstallerBase.cs
@@ -7,7 +7,13 @@ namespace Zenject
 {
     // We'd prefer to make this abstract but Unity 5.3.5 has a bug where references
     // can get lost during compile errors for classes that are abstract
-    public class ScriptableObjectInstallerBase : ScriptableObject, IInstaller
+    public class ScriptableObjectInstallerBase :
+#if ODIN_INSPECTOR
+        SerializedScriptableObject
+#else
+        ScriptableObject
+#endif
+        , IInstaller
     {
         [Inject]
         DiContainer _container = null;
