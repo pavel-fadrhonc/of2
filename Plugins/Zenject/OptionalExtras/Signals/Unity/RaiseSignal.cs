cdcd01333f7ba54c2e7d82f7ba44a28205c2a402
Pavel Fadrhonc <pavel.fadrhonc@gmail.com>
2021-02-23 15:30:50 +0100

    [Extenject] Ability to raise any signal implementing ISignal from Monobehaviour public method (and therefore UnityEvent) and if Odin included it support any number of Unity Serializable arguments

---
+++ Plugins/Zenject/OptionalExtras/Signals/Unity/RaiseSignal.cs
@@ -0,0 +1,98 @@
+﻿#if ODIN_INSPECTOR
+using Sirenix.Serialization;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Zenject;
+using Object = UnityEngine.Object;
+
+namespace Plugins.Zenject.OptionalExtras.Signals.Unity
+{
+    public interface ISignal {}
+
+    public class RaiseSignal : MonoBehaviour, ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        private string _signalType;
+        private object[] _parameters;
+        
+        [HideInInspector][SerializeField]
+        private byte[] _parametersSerializedBA;
+
+        [SerializeField]
+        private List<Object> referencedObjects = new List<Object>(); 
+
+        private Type _signalTypeClass;
+        private FieldInfo[] _paramFieldInfos;
+
+        [Inject] 
+        private SignalBus _signalBus;
+
+        private bool _init;
+
+        private void OnEnable()
+        {
+            if (!_init)
+                Init();
+        }
+
+    #if ODIN_INSPECTOR
+        [Button]
+    #endif
+        public void Raise()
+        {
+            var signal = Activator.CreateInstance(_signalTypeClass);
+
+            for (var index = 0; index < _paramFieldInfos.Length; index++)
+            {
+                var signalField = _paramFieldInfos[index];
+                if (_parameters.Length > index)
+                {
+                    signalField.SetValue(signal, _parameters[index]);
+                }
+            }
+
+            _signalBus.Fire(signal);
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (!_init)
+                Init();
+
+            if (!_init)
+                return;
+
+    #if ODIN_INSPECTOR
+            referencedObjects.Clear();
+            _parametersSerializedBA =
+     SerializationUtility.SerializeValueWeak(_parameters as object, DataFormat.JSON, out referencedObjects);
+    #endif
+        }
+
+        public void OnAfterDeserialize()
+        {
+    #if ODIN_INSPECTOR        
+            _parameters =
+                SerializationUtility.DeserializeValueWeak(_parametersSerializedBA, DataFormat.JSON, referencedObjects) as object[];
+    #endif
+        }
+
+        public void Init()
+        {
+            if (_signalType == null)
+                return;
+            
+            _signalTypeClass = Type.GetType(_signalType);
+            _paramFieldInfos = _signalTypeClass.GetFields();
+
+            if (_parameters == null)
+                _parameters = new object[_paramFieldInfos.Length];
+            
+            _init = true;
+        }
+    }
+}
+
