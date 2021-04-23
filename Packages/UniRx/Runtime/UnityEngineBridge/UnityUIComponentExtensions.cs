24f36aa7fdf71eceda7427a655e3805dec55a850
Pavel Fadrhonc <pavel.fadrhonc@gmail.com>
2021-04-24 00:20:03 +0200

    [UniRx] added support for TMP texts

--- Packages/UniRx/Runtime/UnityEngineBridge/UnityUIComponentExtensions.cs
+++ Packages/UniRx/Runtime/UnityEngineBridge/UnityUIComponentExtensions.cs
@@ -5,6 +5,10 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+#if TMP_PRESENT
+using TMPro;
+#endif
+
 namespace UniRx
 {
     public static partial class UnityUIComponentExtensions
@@ -24,6 +28,40 @@ namespace UniRx
             return source.SubscribeWithState2(text, selector, (x, t, s) => t.text = s(x));
         }
 
+#if TMP_PRESENT
+        
+        public static IDisposable SubscribeToText(this IObservable<string> source, TextMeshPro text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x);
+        }
+
+        public static IDisposable SubscribeToText<T>(this IObservable<T> source, TextMeshPro text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x.ToString());
+        }
+
+        public static IDisposable SubscribeToText<T>(this IObservable<T> source, TextMeshPro text, Func<T, string> selector)
+        {
+            return source.SubscribeWithState2(text, selector, (x, t, s) => t.text = s(x));
+        }  
+        
+        public static IDisposable SubscribeToText(this IObservable<string> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x);
+        }
+
+        public static IDisposable SubscribeToText<T>(this IObservable<T> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x.ToString());
+        }
+
+        public static IDisposable SubscribeToText<T>(this IObservable<T> source, TextMeshProUGUI text, Func<T, string> selector)
+        {
+            return source.SubscribeWithState2(text, selector, (x, t, s) => t.text = s(x));
+        }      
+    
+#endif
+        
         public static IDisposable SubscribeToInteractable(this IObservable<bool> source, Selectable selectable)
         {
             return source.SubscribeWithState(selectable, (x, s) => s.interactable = x);
